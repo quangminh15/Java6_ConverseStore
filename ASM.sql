245aa4082fce65b3937c602935391fe56c5385f7
quangminh15 <phamm7696@gmail.com>
2023-07-04 21:23:47 +0700

    [SQL] Tổng hợp file và sửa các lỗi

--- ASM.sql
+++ ASM.sql
@@ -1,8 +1,9 @@
-﻿Use master;
+﻿go
+Use master;
 go
-create database shopgiay14;
+create database shopgiay19;
 go
-use shopgiay14;
+use shopgiay19;
 go
 -- Tạo bảng Brands
 CREATE TABLE brands (
@@ -23,14 +24,14 @@ go
 -- Tạo bảng Colors
 CREATE TABLE colors (
   color_id BIGINT PRIMARY KEY IDENTITY(1,1),
-  color_name VARCHAR(50) NOT NULL
+  color_name VARCHAR(50) NOT NULL,
   color_activities BIT NOT NULL DEFAULT 0
 );
 go
 -- Tạo bảng Sizes
 CREATE TABLE sizes (
   size_id BIGINT PRIMARY KEY IDENTITY(1,1),
-  size_number FLOAT NOT NULL
+  size_number FLOAT NOT NULL,
   size_activities BIT NOT NULL DEFAULT 0
 );
 go
@@ -38,13 +39,14 @@ go
 CREATE TABLE products (
   product_id BIGINT PRIMARY KEY IDENTITY(1,1),
   product_name VARCHAR(255) NOT NULL,
-  category_id INT NOT NULL,
-  brand_id INT NOT NULL,
+  category_id BIGINT NOT NULL,
+  brand_id BIGINT NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
   product_image1 VARCHAR(50) NOT NULL,
   product_image2 VARCHAR(50) NOT NULL,
   product_image3 VARCHAR(50) NOT NULL,
-  product_activities BIT NOT NULL DEFAULT 0
+  product_activities BIT NOT NULL DEFAULT 0,
+  create_date datetime,
   CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(category_id),
   CONSTRAINT fk_brand FOREIGN KEY (brand_id) REFERENCES brands(brand_id)
 );
@@ -62,8 +64,8 @@ CREATE TABLE product_variants (
 );
 go
 -- Tạo bảng Customers
-CREATE TABLE Customers (
-  customer_id INT PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE customers (
+  customer_id BIGINT PRIMARY KEY IDENTITY(1,1),
   customer_name NVARCHAR(255),
   customer_address NVARCHAR(255),
   customer_phone VARCHAR(20),
@@ -75,8 +77,8 @@ CREATE TABLE Customers (
 );
 go
 -- Tạo bảng Employees
-CREATE TABLE Employees (
-  employee_id INT PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE employees (
+  employee_id BIGINT PRIMARY KEY IDENTITY(1,1),
   employee_name NVARCHAR(255),
   employee_address NVARCHAR(255),
   employee_phone VARCHAR(20),
@@ -90,18 +92,18 @@ CREATE TABLE Employees (
 go
 -- Tạo bảng Orders
 CREATE TABLE orders (
-  order_id BigINT PRIMARY KEY,
-  customer_id BigINT,
-  employee_id BigINT,
+  order_id BIGINT PRIMARY KEY IDENTITY(1,1),
+  customer_id BIGINT,
+  employee_id BIGINT,
   orderdate DATE,
   total float,
-  CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
-  CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+  CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+  CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
 );
 go
 -- Tạo bảng OrderDetails
 CREATE TABLE orderdetails (
-  orderdetails_id BigINT not null primary key,
+  orderdetails_id BigINT not null primary key IDENTITY(1,1),
   order_id BigINT,
   variant_id BigINT,
   Quantity INT,
@@ -111,22 +113,21 @@ CREATE TABLE orderdetails (
 );
 -- Tạo bảng cart
 
-CREATE TABLE cart (
-  cart_id BigINT,
+CREATE TABLE carts (
+  cart_id BigINT primary key IDENTITY(1,1),
   customer_id BigINT,
   variant_id BigINT,
   quantity INT,
-  
-  CONSTRAINT fk_order FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
-  CONSTRAINT fk_variant FOREIGN KEY (variant_id) REFERENCES product_variants(variant_id)
+  CONSTRAINT fk_orderss FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+  CONSTRAINT fk_variantss FOREIGN KEY (variant_id) REFERENCES product_variants(variant_id)
 );
 go
 -- Tạo bảng Promotions
 CREATE TABLE promotions (
-  promotionid INT PRIMARY KEY,
-  promotionname VARCHAR(255),
-  startdate DATE,
-  enddate DATE,
+  promotion_id BIGINT PRIMARY KEY IDENTITY(1,1),
+  promotion_name VARCHAR(255),
+  [start_date] DATE,
+  end_date DATE,
   statuss NVARCHAR(50)
 );
 go
@@ -154,35 +155,37 @@ CREATE TABLE comments (
 go
 -- Tạo bảng Promotions_Products
 CREATE TABLE promotions_products (
-  promotionid INT,
-  productid INT,
-  CONSTRAINT fk_promotion FOREIGN KEY (promotionid) REFERENCES promotions(promotionid),
-  CONSTRAINT fk_product_promotion FOREIGN KEY (productid) REFERENCES products(productid)
+  promotion_product_id BIGINT PRIMARY KEY IDENTITY(1,1),
+  promotion_id BIGINT,
+  product_id BIGINT,
+  CONSTRAINT fk_promotion FOREIGN KEY (promotion_id) REFERENCES promotions(promotion_id),
+  CONSTRAINT fk_product_promotion FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 go
 -- Tạo bảng Favorites
 CREATE TABLE favorites (
-  customerid INT,
-  productid INT,
-  datelike DATE,
-  CONSTRAINT fk_customer_favorite FOREIGN KEY (customerid) REFERENCES customers(customerid),
-  CONSTRAINT fk_product_favorite FOREIGN KEY (productid) REFERENCES products(productid)
+  favorite_id BIGINT PRIMARY KEY IDENTITY(1,1),
+  customer_id BIGINT,
+  product_id BIGINT,
+  date_like DATE,
+  CONSTRAINT fk_customer_favorite FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+  CONSTRAINT fk_product_favorite FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 go
 -- Tạo bảng Suppliers
 CREATE TABLE suppliers (
-  supplierid INT PRIMARY KEY,
-  suppliername VARCHAR(255),
+  supplier_id BIGINT PRIMARY KEY IDENTITY(1,1),
+  supplier_name VARCHAR(255),
   addresss VARCHAR(255),
   phone VARCHAR(20),
   statuss BIT
 );
 go
 -- Tạo bảng SupplierProducts
-CREATE TABLE supplierproducts (
-  supplierid INT,
-  productid INT,
-  CONSTRAINT fk_supplier FOREIGN KEY (supplierid) REFERENCES suppliers(supplierid),
-  CONSTRAINT fk_product_supplier FOREIGN KEY (ProductID) REFERENCES products(productid)
+CREATE TABLE supplier_products (
+  supplier_product_id BIGINT PRIMARY KEY IDENTITY(1,1),
+  supplier_id BIGINT,
+  product_id BIGINT,
+  CONSTRAINT fk_supplier FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id),
+  CONSTRAINT fk_product_supplier FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
-
