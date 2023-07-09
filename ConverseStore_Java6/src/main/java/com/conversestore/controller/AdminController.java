b2eb8de2822b37110c01cdff607eafe3a40a67fc
quangminh15 <phamm7696@gmail.com>
2023-07-09 19:00:41 +0700

    [ECLIPSE] Create giao diện admin

---
+++ ConverseStore_Java6/src/main/java/com/conversestore/controller/AdminController.java
@@ -0,0 +1,42 @@
+package com.conversestore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	
+//	QuangMinh Start
+	@RequestMapping("/admin_danhmuc")
+	public String DanhMucsp() {
+		return "admin/admin_DanhMuc";
+	}
+	
+	@RequestMapping("/admin_thuonghieu")
+	public String ThuongHieu() {
+		return "admin/admin_ThuongHieu";
+	}
+	
+	@RequestMapping("/admin_sanpham")
+	public String SanPham() {
+		return "admin/admin_SanPham";
+	}
+	
+	@RequestMapping("/admin_sanphamCT")
+	public String sanPhamCT() {
+		return "admin/admin_SanPhamCT";
+	}
+	
+	@RequestMapping("/admin_mau")
+	public String mauSP() {
+		return "admin/admin_Mau";
+	}
+	
+	@RequestMapping("/admin_size")
+	public String SizeSP() {
+		return "admin/admin_Size";
+	}
+	
+//	QuangMinh End
+}
